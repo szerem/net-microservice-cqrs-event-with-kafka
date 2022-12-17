4cd8aeff5f4e5f3bb7da29bf2f8b8b56ef9c525f
Krzysztof <szerem@users.noreply.github.com>
2022-12-17 21:12:22 +0100

    EventStoreRepository

--- SM-Post/Post.Cmd/Post.Cmd.Api/Program.cs
+++ SM-Post/Post.Cmd/Post.Cmd.Api/Program.cs
@@ -1,6 +1,12 @@
+using CQRS.Core.Domain;
+using Post.Cmd.Infrastructure.Config;
+using Post.Cmd.Infrastructure.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.Configure<MongoDBConfig>(builder.Configuration.GetSection(nameof(MongoDBConfig)));
+builder.Services.AddScoped<IEventStoreRepository, EventStoreRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
